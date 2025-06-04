b017531eb65230113772382fd0908def820cf2a7
KJ Roelke <kj.roelke@gmail.com>
2025-06-03 21:09:14 -0500

    init page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kjroelke.github.io',
-	base: 'astro-starter',
+	base: 'minji-doljanchi',
 	build: {
 		assets: 'dist',
 	},
