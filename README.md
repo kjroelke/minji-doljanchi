5ab6b7162242042d04a49a514a89e34766463ffb
KJ Roelke <kj.roelke@gmail.com>
2025-06-03 20:40:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Astro x Bootstrap 5 Starter
+
+Quick startup for Astro w/ Bun and Bootstrap 5.
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+export const BASE_URL = import.meta.env.BASE_URL;
+export const SITE_TITLE = 'Astro x Bootstrap';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
